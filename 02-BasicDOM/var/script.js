aeacc50637e497d6a4b64308516898a9f556c154
tati oliveira <tat1roliv@gmail.com>
2022-01-20 20:39:07 -0300

    Update script.js

--- 02-BasicDOM/var/script.js
+++ 02-BasicDOM/var/script.js
@@ -13,6 +13,10 @@ titulo.innerText; // retorna o texto;
 titulo.classList; // retorna as classes;
 titulo.id; // retorna o id;
 titulo.offsetHeight; // retorna a altura do elemento;
-titulo.addEventListener('click', callback);
+
 // ativa a função callback ao click no titulo
+titulo.addEventListener('click', function() {
+  console.log('clicou em', titulo.innerText);
+});
+
 
