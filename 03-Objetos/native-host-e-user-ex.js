3876017252df567db4c1b7597e36ab15f60cf9d5
tati oliveira <tat1roliv@gmail.com>
2022-02-10 19:42:25 -0300

    host | browser

---
+++ 03-Objetos/native-host-e-user-ex.js
@@ -0,0 +1,20 @@
+
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
+String
+Date
+
+// Liste 5 objetos do browser
+NodeList
+HTMLCollection
+Element
+Window
+Document
+
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
---
+++ 03-Objetos/native-host-e-user.js
@@ -0,0 +1,16 @@
+// Construtores de objetos nativos
+Object
+String
+Array
+Function
+
+
+// Objetos do browser
+NodeList
+HTMLCollection
+Element
+
+
+//verificacao se existe
+if (typeof Array.from !== "undefined");
+if (typeof NodeList !== "undefined");
