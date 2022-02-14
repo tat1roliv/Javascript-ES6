cc236dad4c3a19f1c470735d62caf8d7a46b4e1c
tati oliveira <tat1roliv@gmail.com>
2022-02-14 20:26:06 -0300

    strings

--- 03-Objetos/string-ex.js
+++ 03-Objetos/string-ex.js
@@ -23,28 +23,75 @@ const transacoes = [
     },
   ];
   
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+
+transacoes.forEach((obj) => {
+    
+    const numeroTratado = +obj.valor.replace('R$ ', '');//converte em string e retira txt
+
+    if(obj.descricao.slice(0,4) === 'Taxa'){
+        taxaTotal = taxaTotal + numeroTratado;
+    }
+
+    else if(obj.descricao.slice(0,11) === 'Recebimento'){
+    recebimentoTotal = recebimentoTotal + numeroTratado;
+    }
+
+});
+
+console.log(taxaTotal); 
+console.log(recebimentoTotal);
   
   
-  // Retorne uma array com a lista abaixo
-  const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
   
-  const arrayTransportes = transportes.split(';');
-  console.log(arrayTransportes);
-  
-  // Substitua todos os span's por a's
-  let html = `<ul>
-                  <li><span>Sobre</span></li>
-                  <li><span>Produtos</span></li>
-                  <li><span>Contato</span></li>
-                </ul>`;
-  
-  
-  // Retorne o último caracter da frase
-  const frase = 'Melhor do ano!';
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+
+const arrayTransportes = transportes.split(';');
+console.log(arrayTransportes);
   
 
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+            </ul>`;
+
+const htmlText = html;
+const htmlArray = htmlText.split('span');
+const htmlNovo = htmlArray.join('a');
+
+console.log(htmlNovo);
+
+               
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+console.log(frase.slice(-1));
   
-  // Retorne o total de taxas
-  const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
-  
+
+
+// Retorne o total de taxas
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+
+const sexo1 = 'Feminino';
+const sexo2 = 'feminino';
+const sexo3 = 'FEMININO';
+(sexo1.toLowerCase() === 'feminino'); // true
+(sexo2.toLowerCase() === 'feminino'); // true
+(sexo3.toLowerCase() === 'feminino'); // true
+
+let taxasTotalTransacoes = 0;
+transacoes2.forEach((elemento) => {
+    elemento = elemento.toLowerCase();
+    elemento = elemento.trim();
+    
+    if(elemento.slice(0,4) === 'taxa'){
+        taxasTotalTransacoes = taxasTotalTransacoes + 1;
+    }
+
+})
+
+console.log(taxasTotalTransacoes);
   
