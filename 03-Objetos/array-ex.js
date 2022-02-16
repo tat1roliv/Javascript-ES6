fd4bd891f3f476eceb6d6ea0bfb2341ddf906b31
tati oliveira <tat1roliv@gmail.com>
2022-02-16 20:24:37 -0300

    arrays 2

---
+++ 03-Objetos/array-ex.js
@@ -0,0 +1,54 @@
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const comidasMenosPrimeiro = comidas.slice(1, comidas.length); //.shift()
+console.log(comidasMenosPrimeiro);
+
+// Remova o último valor de comidas e coloque em uma variável
+const comidasMenosUltimo = comidas.pop();
+console.log(comidasMenosUltimo);
+console.log(comidas);
+
+
+// Adicione 'Arroz' ao final da array
+
+
+
+// Adicione 'Peixe' e 'Batata' ao início da array
+
+
+
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber',
+'Julia'];
+// Arrume os estudantes em ordem alfabética
+
+
+
+
+// Inverta a ordem dos estudantes
+
+
+
+
+
+// Verifique se Joana faz parte dos estudantes
+
+
+
+
+
+// Verifique se Juliana faz parte dos estudantes
+let html = `<section>
+ <div>Sobre</div>
+ <div>Produtos</div>
+ <div>Contato</div>
+ </section>`
+
+
+// Substitua section por ul e div com li,
+// utilizando split e join
+
+
+
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover salve a array original em outra variáve
--- 03-Objetos/array.js
+++ 03-Objetos/array.js
@@ -96,3 +96,85 @@ carros; // ['Fiat', 'VW'];
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
 carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
 
+
+
+
+
+//[].splice()
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+carros.splice(1, 0, 'Kia', 'Mustang'); // []
+carros; // ['Ford', 'Kia', 'Mustang', 'Fiat', 'VW', 'Honda']
+carros.splice(3, 2, 'Ferrari'); // ['Fiat', 'VW']
+carros; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
+
+
+
+
+//[].copyWithin()
+['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(2, 0, 3);//target, copy, 
+// ['Item1', 'Item2', 'Item1', 'Item2']
+['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(-1);
+// ['Item1', 'Item2', 'Item3', 'Item1']
+
+
+
+
+
+//[].fill() 
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana');
+// ['Banana', 'Banana', 'Banana', 'Banana']
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 2);
+// ['Item1', 'Item2', 'Banana', 'Banana']
+['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 1, 3);
+// ['Item1', 'Banana', 'Banana', 'Item4']
+
+
+
+
+
+//Métodos de Acesso [].concat()
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+const transportes = transporte1.concat(transporte2);
+// ['Barco', 'Aviao', 'Carro', 'Moto'];
+const maisTransportes = [].concat(transporte1, transporte2,
+'Van');
+// ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+
+
+
+
+
+
+//[].includes(), [].indexOf() e [].lastIndexOf()
+const linguagens = ['html', 'css', 'js', 'php', 'python',
+'js'];
+linguagens.includes('css'); // true
+linguagens.includes('ruby'); // false
+linguagens.indexOf('python'); // 4
+linguagens.indexOf('js'); // 2
+linguagens.lastIndexOf('js'); // 5
+
+
+
+
+//[].join()
+const linguagens = ['html', 'css', 'js', 'php', 'python'];
+linguagens.join(); // 'html,css,js,php,python'
+linguagens.join(' '); // 'html css js php python'
+linguagens.join('-_-'); // 'html-_-css-_-js-_-php-_-python'
+let htmlString = '<h2>Título Principal</h2>'
+htmlString = htmlString.split('h2');
+// ['<', '>Título Principal</', '>']
+htmlString = htmlString.join('h1');
+// <h1>Título Principal</h1>
+
+
+
+
+//[].slice()
+
+const linguagens = ['html', 'css', 'js', 'php', 'python'];
+linguagens.slice(3); // ['php', 'python']
+linguagens.slice(1, 4); // ['css', 'js', 'php']
+const cloneLinguagens = linguagens.slice(); //clonar array
