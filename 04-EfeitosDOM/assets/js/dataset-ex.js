ba2dfdf0823dce8d0105b69e67b8bd21a1599ce1
tati oliveira <tat1roliv@gmail.com>
2022-04-14 14:02:57 -0300

    dataset-ex

--- 04-EfeitosDOM/assets/js/dataset-ex.js
+++ 04-EfeitosDOM/assets/js/dataset-ex.js
@@ -1,10 +1,17 @@
 // Adicione um atributo data-anime="show-down" e
 // data-anime="show-right" a todos as section's
 // com descricão dos animais.
+
+/*
+const sectionAnimais = document.querySelectorAll('#animais');
+sectionAnimais.forEach((e) => e.dataset.anime = "show-down");
+*
 // Utilizando estes atributos, adicione a classe
 // show-down ou show-right a sua respectiva section
 // assim que a mesma aparecer na tela (animacao tab)
+
 // No CSS faça com que show-down anime de cima para baixo
 // e show-right continue com a mesma animação da esquerda
 // para a direita
+
 // Substitua todas as classes js- por data atribute
--- 04-EfeitosDOM/assets/js/script.js
+++ 04-EfeitosDOM/assets/js/script.js
@@ -1,5 +1,129 @@
-/*dataset */
-const h1 = document.querySelector('h1');
+/* navegacao por tab */
 
-console.log(Object.prototype.toString.call(h1));
-console.log(h1.dataset);
+function initTabNav(){
+
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('[data-tab="content"] section');
+    
+    if ( tabMenu.length && tabContent.length){ //if true && true
+    
+        function activeTab(index){
+            tabContent.forEach( (section) => {
+                section.classList.remove('ativo');
+            })
+            tabContent[index].classList.add('ativo', tabContent[index].dataset.anime );
+        }
+        
+        tabMenu.forEach( (itemMenu, index) => {
+            itemMenu.addEventListener( 'click', () => {
+                activeTab(index);
+            });
+        })
+    
+    }
+
+
+}
+
+initTabNav();
+
+
+
+
+/* accordion list */
+
+function initAccordion(){
+
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+
+    if (accordionList.length){
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+        
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion );
+        });
+
+    }
+
+
+}
+
+initAccordion();
+
+
+
+
+
+/* scroll suave */
+/*chrome + firefox*/
+
+function initScrollSuave(){
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"');
+
+    function scrollToSection(e){
+        e.preventDefault();
+        const href = e.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+    
+       // const sectionTop = section.offsetTop;
+       // window.scrollTo(0, sectionTop);
+    
+       section.scrollIntoView({
+           behavior: 'smooth',
+           block: 'start',
+       });
+    
+    }
+    
+    linksInternos.forEach((element) => {
+        element.addEventListener('click', scrollToSection);
+    });
+
+}
+
+initScrollSuave();
+
+
+
+
+
+/* scroll animation */
+
+const sections = document.querySelectorAll('.js-scroll');
+
+function initAnimacaoScroll(){
+
+    if (sections.length){
+
+        const windowMetade =  window.innerHeight * 0.6;
+    
+        function animaScroll(){
+            sections.forEach( (section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade ) < 0;
+        
+                if (isSectionVisible){
+                    section.classList.add('ativa');
+                } else {
+                    section.classList.remove('ativa');
+                }
+            })
+        }
+        
+        animaScroll();
+        
+        window.addEventListener('scroll', animaScroll);
+    }
+    
+
+}
+
+initAnimacaoScroll();
