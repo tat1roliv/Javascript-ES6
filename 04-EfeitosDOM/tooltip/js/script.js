f21f1da4d8f342146da16e5e7d9cf38f630b2325
tati oliveira <tat1roliv@gmail.com>
2022-04-14 17:48:40 -0300

    ++ tooltip base

--- 04-EfeitosDOM/tooltip/js/script.js
+++ 04-EfeitosDOM/tooltip/js/script.js
@@ -2,15 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import * as teste from './modules/teste.js';
-
-teste.teste1()
-teste.teste2()
-console.log(teste.senha);
-
-console.log(this)
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
