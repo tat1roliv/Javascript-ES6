fc70d568ac53dd1537fb063d0a245148c2c9905c
tati oliveira <tat1roliv@gmail.com>
2022-04-19 17:06:15 -0300

    ++folder

--- 04-EfeitosDOM/dropdown/js/modules/clickOutside.js
+++ 04-EfeitosDOM/dropdown/js/modules/clickOutside.js
@@ -1,23 +1,27 @@
 
 
-export default function clickOutside(element, events, callback){
-    const html = document.documentElement;
-    const outside = 'data-outside';
+export default function initClickOutside(){
 
-    if(!element.hasAttribute(outside)){
-        events.forEach( userEvent => {
-            html.addEventListener(userEvent, handleOutsideClick);
-        })
-        element.setAttribute(outside, '');
-    }
-
-    function handleOutsideClick(event){
-        if(!element.contains(event.target)){
-            element.removeAttribute(outside);
+    function clickOutside(element, events, callback){
+        const html = document.documentElement;
+        const outside = 'data-outside';
+    
+        if(!element.hasAttribute(outside)){
             events.forEach( userEvent => {
-                html.removeEventListener(userEvent, handleOutsideClick);
+                html.addEventListener(userEvent, handleOutsideClick);
             })
-            callback();        
+            element.setAttribute(outside, '');
+        }
+    
+        function handleOutsideClick(event){
+            if(!element.contains(event.target)){
+                element.removeAttribute(outside);
+                events.forEach( userEvent => {
+                    html.removeEventListener(userEvent, handleOutsideClick);
+                })
+                callback();        
+            }
         }
     }
+    
 }
