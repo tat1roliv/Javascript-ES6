73bebd444e843f7e4c60bae31d297e4c3857784d
tati oliveira <tat1roliv@gmail.com>
2022-06-17 18:36:30 -0300

    anima numeros

--- 04-EfeitosDOM/anim-numeros/js/modules/anima-numeros.js
+++ 04-EfeitosDOM/anim-numeros/js/modules/anima-numeros.js
@@ -1,42 +1,31 @@
-export default function initAnimaNumeros(){
-
-
-    function animaNumeros(){
-
-        const numeros = document.querySelectorAll('[data-numero]');
-        //console.log(numeros);
-        
-        numeros.forEach((numero) => {
-            const total = numero.innerText;
-            let start = 0;
-        
-            const incremento = Math.floor(total / 100);
-        
-            const timer = setInterval(() => {
-                //start++;
-                start = start + incremento;
-                numero.innerText = start;
-                if(start >= total){
-                    numero.innerText = total; /* por conta do incremento, no caso de retornar n maior que o numero original*/
-                    clearInterval(timer);
-                }
-            }, 25 * Math.random());
-        
-        })
-        
+export default function initAnimaNumeros() {
+  function animaNumeros() {
+    const numeros = document.querySelectorAll('[data-numero]');
+  
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const inscremento = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start = start + inscremento;
+        numero.innerText = start;
+        if(start > total) {
+          numero.innerText = total;
+          clearInterval(timer);
         }
-        
-        function handleMutation(mutation){
-            if(mutation[0].target.classList.contains('ativo')){
-                observer.disconnect();
-                animaNumeros();
-            }
-        
-        }
-        
-        const observerTarget = document.querySelector('.numeros');
-        const observer = new MutableObserver(handleMutation);
-        observer.observe(observerTarget , {attributes: true});
-
+      }, 25 * Math.random());
+    });
+  }
+  
+  function handleMutation(mutation) {
+   if(mutation[0].target.classList.contains('ativo')) {
+    observer.disconnect();
+    animaNumeros();
+   }
+  }
+  
+  const observerTarget = document.querySelector('.numeros');
+  const observer = new MutationObserver(handleMutation);
+  
+  observer.observe(observerTarget, {attributes: true});
 }
-
--- 04-EfeitosDOM/anim-numeros/js/modules/scroll-animacao.js
+++ 04-EfeitosDOM/anim-numeros/js/modules/scroll-animacao.js
@@ -9,7 +9,7 @@ export default function initAnimacaoScroll() {
         const isSectionVisible = (sectionTop - windowMetade) < 0;
         if(isSectionVisible)
           section.classList.add('ativo');
-        else if( section.classList.contains('ativo')){
+        else if(section.classList.contains('ativo')) {
           section.classList.remove('ativo');
         }
       })
