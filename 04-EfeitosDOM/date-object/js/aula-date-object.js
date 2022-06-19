cc4c5674b246e9b46646cea0458ca1ab2fdb581c
tati oliveira <tat1roliv@gmail.com>
2022-06-19 15:56:31 -0300

    ajustes time

--- 04-EfeitosDOM/date-object/js/aula-date-object.js
+++ 04-EfeitosDOM/date-object/js/aula-date-object.js
@@ -4,11 +4,11 @@ agora;
 // Semana Mês Dia Ano HH:MM:SS GMT
 agora.getDate() ;// Dia
 agora.getDay() ;// Dia da Semana ex: 5 = Fri
-agora.getMonth() ;// Número dia mês
+agora.getMonth() ;// Número dia mês 0-11
 agora.getFullYear(); // Ano
 agora.getHours(); // Hora
 agora.getMinutes() ;// Minutos
-agora.getTime() ;// ms desde 1970
+agora.getTime() ;// ms desde 1 jan 1970
 agora.getUTCHours() - 3 ; // Brasília
 
 
@@ -20,7 +20,7 @@ const diasPassados = tempo.getTime() / (24 * 60 * 60 * 1000);
 
 
 
-/*data futuracion */
+/*data futura / dias ate data */
 const agora2 = new Date();
 const promocao = new Date('December 24 2018 23:59');
 function converterEmDias(time) {
--- 04-EfeitosDOM/date-object/js/modules/funcionamento.js
+++ 04-EfeitosDOM/date-object/js/modules/funcionamento.js
@@ -1,14 +1,16 @@
 export default function initFuncionamento() {
   const funcionamento = document.querySelector('[data-semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);//conv em array + conv string to number
   const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
   
   const dataAgora = new Date();
   const diaAgora = dataAgora.getDay();
   const horarioAgora = dataAgora.getHours();
   
+  //const teste = [1,2,3,4,5].indexOf(5);//4
+   //const teste = [1,2,3,4,5].indexOf(6);//-1
   const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
-  const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);
+  const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1]);// < ao inves de <= , para o caso de ej 18h30
   
   if(semanaAberto && horarioAberto) {
     funcionamento.classList.add('aberto');
