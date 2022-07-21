9f67581a661b66cbbd69cd478355a38b892df346
tati oliveira <tat1roliv@gmail.com>
2022-07-21 08:31:26 -0300

    operadores

---
+++ Rocketseat/operadores-aritimeticos.js
@@ -0,0 +1,25 @@
+/*
+* operadores aritimeticos
+*/
+
+/* resto da divisão: sinal % */
+let remainder
+remainder = 11 % 9
+console.log(remainder)
+
+/* incremento: sinal ++ */
+let increment = 0
+
+
+/* ++variavel vs variavel++ */
+//console.log(increment++)
+console.log(++increment)
+console.log(increment)
+
+/* decremento: sinal -- */
+let decrement = 0
+decrement--
+console.log(decrement)
+
+/* exponencial: sinal ** */
+console.log(3 ** 3)
