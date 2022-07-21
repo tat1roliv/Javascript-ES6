37ba998da250f0bb8ea7b7241695a5e75048bfc2
tati oliveira <tat1roliv@gmail.com>
2022-07-21 09:23:52 -0300

    controle de fluxo

---
+++ Rocketseat/controle-fluxo.js
@@ -0,0 +1,84 @@
+/*
+CONTROLE FLUXO
+*/
+
+
+/*if else*/
+let temperature = 36.9
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37.5 && temperature >= 37
+
+if(highTemperature) {
+    console.log('Febre alta')
+} else if(mediumTemperature) {
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}
+
+
+/* switch*/
+let expression = ''
+
+switch (expression) { // puxa a expressão para o switch
+  case 'a': // confere se o valor da expressão é o correto
+    console.log('a')
+    break // para a execução do switch apenas se verdadeiro
+  case 'b':
+    console.log('b')
+    break
+  default: // caso nenhum valor seja o correto, realizará a 
+					 //instrução dentro de si.
+    console.log('default')
+    break
+}
+
+
+function calculate(number1, operator, number2) {
+    let result = 0;
+
+    switch (operator) {
+        case '+':
+            result = number1 + number2
+            break
+        case '-':
+            result = number1 - number2 
+            break
+        case '*':
+            result = number1 * number2
+            break
+        case '/':
+            result = number1 / number2 
+            break
+        default:
+            console.log('não implementado')
+            break
+    }
+
+    return result
+}
+
+console.log(calculate(4, '%', 8))
+console.log(calculate(4, '+', 8))
+
+
+
+/* throw */
+
+function sayMyName(name = '') {
+    if (name === '') {
+        throw new Error('Nome é obrigatório');
+    }
+
+    console.log('depois do erro')//nao exibe
+}
+
+/* try...catch */
+
+try {
+    sayMyName()
+} catch (e) {
+    console.log(e)
+}
+
+console.log('depois do erro2')//exibe
