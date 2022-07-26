3601484325206b28edfcd2f252f400e510926892
tati oliveira <tat1roliv@gmail.com>
2022-07-26 09:14:12 -0300

    dom selectors => return type

---
+++ Rocketseat/dom-select-elements.js
@@ -0,0 +1,23 @@
+/*
+DOM 
+Select elements
+*/
+
+//getElementById - return element
+console.log(document.getElementById('title'));
+
+//getElementsByClassName - return html collection
+console.log(document.getElementsByClassName('single-p'));
+
+//getElementsByTagName - return html collection
+console.log(document.getElementsByTagName('h1'));
+
+//querySelector - return element
+console.log(document.querySelector('.title'));
+
+//querySelectorAll - return nodeList
+console.log(document.querySelectorAll('.title'));
+
+
+//nodeList vs html collection
+//nodeList array-like
