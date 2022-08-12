214b73909f348818139314812263e7b527564fce
tati oliveira <tat1roliv@gmail.com>
2022-08-12 08:51:03 -0300

    node ejs foreach tela

--- node-ejs/server.js
+++ node-ejs/server.js
@@ -9,7 +9,36 @@ app.set("view engine", "ejs");
 
 //rotas
 app.get("/", function(req, res){
-    res.render("pages/index");//index.ejs
+    const items = [
+        { 
+            "title": "D",
+            "message": "teste teste 1"
+        },
+        { 
+            "title": "E",
+            "message": "teste teste 2"
+        },
+        { 
+            "title": "M",
+            "message": "teste teste 3"
+        },
+        { 
+            "title": "A",
+            "message": "teste teste 4"
+        },
+        { 
+            "title": "I",
+            "message": "teste teste 5"
+        },
+        { 
+            "title": "S",
+            "message": "teste teste 6"
+        }
+    ];
+    res.render("pages/index" ,
+        { 
+            information : items 
+        });//index.ejs
 })
 
 app.get("/sobre", function(req, res){
